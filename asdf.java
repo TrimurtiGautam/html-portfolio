14067262266127cadd3d1567f06c092e5ab789da
TrimurtiGautam <144714741+TrimurtiGautam@users.noreply.github.com>
2024-01-15 01:48:44 +0530

    Add files via upload

---
+++ asdf.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.* ;
+
+public class Solution {
+    public static void setZeros(int matrix[][]) {
+        HashSet<Integer> row=new HashSet<>();
+        HashSet<Integer> col=new HashSet<>();
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if (matrix[i][j]==0){
+                    row.add(i);
+                    row.add(j);
+                }
+            }
+        }
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(row.contains(i)||col.contains(j)){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+}
